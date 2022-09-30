b37e09fcd1200c30124c1ebc0edd316edd57bb96
osck <kamil555331@o2.pl>
2022-09-30 19:49:20 +0200

    nav added

--- js/main.js
+++ js/main.js
@@ -1,9 +1,23 @@
+const nav = document.querySelector('.nav');
+const navBtn = document.querySelector('.burger-btn');
+const allNavItems = document.querySelectorAll('.nav__item')
 const footerYear = document.querySelector('.footer__year')
 
+const handleNav = () => {
+	nav.classList.toggle('nav--active')
+	allNavItems.forEach(item => {
+		item.addEventListener('click', () => {
+			nav.classList.remove('nav--active')
+		})
+	})
+}
+
+navBtn.addEventListener('click', handleNav)
 
 const handleCurrentYear = () => {
 	const year = new Date().getFullYear()
 	footerYear.innerText = year
 }
+
 
 handleCurrentYear();
