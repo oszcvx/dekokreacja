988352f54692b91039589ba584bf68a9c47c2c09
osck <kamil555331@o2.pl>
2022-10-24 22:58:26 +0200

    bootstrap nav updated

--- js/main.js
+++ js/main.js
@@ -1,33 +1,9 @@
-const nav = document.querySelector('.nav')
-const navBtn = document.querySelector('.burger-btn')
-const allNavItems = document.querySelectorAll('.nav__item')
 const footerYear = document.querySelector('.footer__year')
 
-const handleNav = () => {
-	nav.classList.toggle('nav--active')
-	allNavItems.forEach(item => {
-		item.addEventListener('click', () => {
-			nav.classList.remove('nav--active')
-		})
-	})
-
-	handleNavItemsAnimation()
-}
-
-const handleNavItemsAnimation = () => {
-	let delayTime = 0
-
-	allNavItems.forEach(item => {
-		item.classList.toggle('nav-items-animation')
-		item.style.animationDelay = '.' + delayTime + 's';
-		delayTime++;
-	})
-}
 
 const handleCurrentYear = () => {
 	const year = new Date().getFullYear()
 	footerYear.innerText = year
 }
 
-navBtn.addEventListener('click', handleNav)
 handleCurrentYear()
