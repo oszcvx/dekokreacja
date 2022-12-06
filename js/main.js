192d44ff638260f53d1a96c5535ce328ccad3776
osck <kamil555331@o2.pl>
2022-12-06 22:15:08 +0100

    nav repaired

--- js/main.js
+++ js/main.js
@@ -1,5 +1,12 @@
-const footerYear = document.querySelector('.footer__year')
+	const nav = document.querySelector('.navbar');
+	const allNavItems = document.querySelectorAll('.nav-link');
+	const navList = document.querySelector('.navbar-collapse');
+	const footerYear = document.querySelector('.footer__year')
 
+  
+	allNavItems.forEach((item) =>
+	  item.addEventListener('click', () => navList.classList.remove('show'))
+	)
 
 const handleCurrentYear = () => {
 	const year = new Date().getFullYear()
